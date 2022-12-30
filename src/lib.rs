5b33fbd423ecec1cec306f9880398d6f9bdacd2c
Tobias Waurick <tobtherock@proton.me>
2022-12-30 17:59:15 +0100

    feat: added API draft

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,34 @@
 pub mod measurement_matrix;
 pub mod transform_matrix;
+
+pub struct Model<const M: usize, const N: usize> {}
+
+impl<const M: usize, const N: usize> Model<M, N> {
+    pub fn new() -> Self {
+        todo!()
+    }
+
+    pub fn with_seed(seed: usize) -> Self {
+        todo!()
+    }
+
+    pub fn transform_matrix(&mut self) -> &mut Self {
+        todo!()
+    }
+
+    pub fn sensing_matrix(&mut self) -> &mut Self {
+        todo!()
+    }
+
+    pub fn algorithm(&mut self) -> &mut Self {
+        todo!()
+    }
+
+    pub fn compress(&self, input: Vec<f64>) -> Vec<f64> {
+        todo!();
+    }
+
+    pub fn decompress(&self, input: Vec<f64>) -> Vec<f64> {
+        todo!();
+    }
+}
