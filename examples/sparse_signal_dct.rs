5c22bc46484a78ead17440c1777a13c24f1c03d5
Tobias Waurick <tobtherock@proton.me>
2023-03-19 17:39:46 +0100

    feat: use transformation in example

--- examples/sparse_signal_dct.rs
+++ examples/sparse_signal_dct.rs
@@ -1,37 +1,55 @@
 use std::f64::consts::PI;
 
-use plotly::{common::Mode, Plot, Scatter};
-use rand::{distributions::Uniform, Rng};
+use plotly::{
+    common::{Marker, MarkerSymbol, Mode},
+    Plot, Scatter,
+};
+use rand::{
+    distributions::{uniform::SampleUniform, Uniform},
+    Rng,
+};
 use sense_motive::{Model, ModelBuilder, Transformation};
 
-const N: usize = 256;
-const M: usize = 64;
-const K: usize = 12;
+const N: usize = 1024;
+const M: usize = 256;
+const K: usize = 16;
 
-const TRANSFORM: Transformation = Transformation::None;
+const TRANSFORM: Transformation = Transformation::Dct1dInverse;
 
 fn main() {
     let model = ModelBuilder::new()
         .with_transformation(TRANSFORM)
         .build::<M, N>();
-    // model.sensing_matrix().transform_matrix(); // TODO add enums to select algorithm sensing matrix etc.
 
-    // let original = generate_signal(&[0.25, 1.0]);
-    let original = generate_sparse_signal(K);
+    let original = match TRANSFORM {
+        Transformation::None => generate_sparse_signal(K),
+        Transformation::Dct1dInverse => generate_cos_signal(K),
+    };
+
     let compressed = model.compress(&original);
     let decompressed = model.decompress(&compressed);
 
     let mut plot = Plot::new();
-    let trace = Scatter::new((0..N - 1).collect(), original.clone()).mode(Mode::Lines);
+    let trace = Scatter::new((0..N - 1).collect(), original.clone())
+        .name("Original")
+        .mode(Mode::LinesMarkers)
+        .marker(Marker::new().symbol(MarkerSymbol::Cross));
     plot.add_trace(trace);
 
+    // TODO 2nd trace of compressed
     // let trace = Scatter::new((0..M - 1).collect(), compressed).mode(Mode::Lines);
     // plot.add_trace(trace);
 
-    let trace = Scatter::new((0..N - 1).collect(), decompressed.clone()).mode(Mode::Lines);
+    let trace = Scatter::new((0..N - 1).collect(), decompressed.clone())
+        .name("Decompressed")
+        .mode(Mode::LinesMarkersText)
+        .marker(Marker::new().symbol(MarkerSymbol::Circle));
     plot.add_trace(trace);
 
-    println!("Error {}", error_l2(&original, &decompressed));
+    println!(
+        "Error (normalized to N) {}",
+        error_l2(&original, &decompressed) / (N as f64)
+    );
     plot.show();
 }
 
@@ -43,27 +61,25 @@ fn error_l2(original: &Vec<f64>, decompressed: &Vec<f64>) -> f64 {
         .sum()
 }
 
-// TODO amplitudes
-fn generate_signal(frequencies: &[f64]) -> Vec<f64> {
-    let mut signal = vec![0.0; N];
+fn generate_cos_signal(sparsity: usize) -> Vec<f64> {
+    let frequencies = random_values(sparsity, 0.0, 10.0);
+    let amplitudes = random_values(sparsity, 0.0, 1.0);
+    let params: Vec<(f64, f64)> = frequencies.zip(amplitudes).collect();
 
-    for (i, val) in signal.iter_mut().enumerate() {
-        *val = frequencies
-            .iter()
-            .map(|&f| ((i as f64 * 2.0 * PI * f).cos()))
-            .sum();
-    }
-
-    signal
+    (0..N)
+        .map(|i| i as f64 / N as f64)
+        .map(|i| {
+            params
+                .iter()
+                .map(|(f, a)| (a * (i * 2.0 * PI * f).cos()))
+                .sum()
+        })
+        .collect()
 }
 
 fn generate_sparse_signal(sparsity: usize) -> Vec<f64> {
-    let indices = rand::thread_rng()
-        .sample_iter(Uniform::new(0, N))
-        .take(sparsity);
-    let amplitudes = rand::thread_rng()
-        .sample_iter(Uniform::new(0.0, 1.0))
-        .take(sparsity);
+    let indices = random_values(sparsity, 0, N);
+    let amplitudes = random_values(sparsity, 0.0, 1.0);
 
     let mut signal = vec![0.0; N];
     for (i, a) in indices.zip(amplitudes) {
@@ -71,4 +87,13 @@ fn generate_sparse_signal(sparsity: usize) -> Vec<f64> {
     }
 
     signal
+}
+
+fn random_values<T>(n: usize, min: T, max: T) -> impl Iterator<Item = T>
+where
+    T: SampleUniform,
+{
+    rand::thread_rng()
+        .sample_iter(Uniform::new(min, max))
+        .take(n)
 }
